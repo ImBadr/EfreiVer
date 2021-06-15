b2f40b1ec20ac769ae136f132f49d64f46e1504c
Badr TADJER <tadjer.badr@gmail.com>
2021-06-15 14:51:57 +0200

    bootstrap installed

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,6 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+// You can specify which plugins you need
+import { Tooltip, Toast, Popover } from 'bootstrap';
