457b777df5a71f35c1e27fd1c391a9d2578a0d03
Badr TADJER <tadjer.badr@gmail.com>
2021-06-18 15:21:06 +0200

    VueJS + Bootstrap + FontAwesome installed and work

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Default from './components/Default';
+
+new Vue({
+    el: '#app',
+    render: h => h(Default)
+});
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('appVueJs', './assets/js/app.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -70,6 +71,8 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .enableVueLoader()
 
 ;
 
