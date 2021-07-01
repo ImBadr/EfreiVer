486c53e9a9b33720dd2c729df803671e160255ef
Badr TADJER <tadjer.badr@gmail.com>
2021-07-01 15:55:14 +0200

    view bientot fini

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
 use App\Repository\AnnonceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +20,18 @@ class AnnonceController extends AbstractController
 
         return $this->render('annonce/index.html.twig', [
             'annonces' => $annonces
+        ]);
+    }
+
+    /**
+     * @Route("/annonces/{id}/view", name="view_annonce")
+     * @param Annonce $annonce
+     * @return Response
+     */
+    public function show(Annonce $annonce): Response
+    {
+        return $this->render('annonce/annonce.html.twig', [
+            'annonce' => $annonce
         ]);
     }
 }
